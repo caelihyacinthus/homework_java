78cc089b06bde8d1c76e4dba3d81a317eadb8dd7
vejas valiukonis <vejasva@gmail.com>
2025-01-07 14:32:58 +0200

    extra task

---
+++ extra/SwapMath.java
@@ -0,0 +1,12 @@
+public class SwapMath {
+    public static void main(String[] args) {
+        //swap to numbers without temp var
+        int a = 154;
+        int b = 6;
+        System.out.println(a + " " + b);
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println(a + " " + b);
+    }
+}
